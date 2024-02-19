a52dfcc5207dc316901262596c091b958836c561
Iván Máximiliano, Lo Giudice <ivanmaxlogiudice@gmail.com>
2024-02-19 02:29:11 -0300

    feat: add findDevice

--- lib/index.ts
+++ lib/index.ts
@@ -4,3 +4,4 @@ const require = createRequire(import.meta.url)
 const addon = require('../build/Release/npcap.node')
 
 console.log(addon.deviceList())
+console.log(addon.findDevice('192.168.0.6'))
--- src/binding.cc
+++ src/binding.cc
@@ -3,41 +3,57 @@
 #include <windows.h>
 #include <stdio.h>
 #include <winsock2.h>
+#include <string>
 
 static void loadNpcap(napi_env env) {
     char path[MAX_PATH] = {0};
 
-    if (GetSystemDirectoryA(path, MAX_PATH) == 0)
+    if (GetSystemDirectoryA(path, MAX_PATH) == 0) {
         napi_throw_error(env, NULL, "Failed to get the system directory.");
+        return;
+    }
 
     strcat(path, "\\Npcap");
 
-    if (SetDllDirectoryA(path) == 0)
+    if (SetDllDirectoryA(path) == 0) {
         napi_throw_error(env, NULL, "Failed to set the NPCap directory.");
+        return;
+    }
 
-    if (LoadLibraryA("wpcap.dll") == 0)
+    if (LoadLibraryA("wpcap.dll") == 0) {
         napi_throw_error(env, NULL, 
             "\n"
             "  ERROR! Failed to load 'wpcap.dll'\n"
             "  Have you installed the Npcap library? See https://npcap.com/#download\n"
             "\n"
         );
+        return;
+    }
 
     // Notify Npcap version.
     printf("** %s\n", pcap_lib_version());
 }
 
+static const char* GetIpAddress(const struct sockaddr* addr) {
+    char buffer[INET6_ADDRSTRLEN] = {0};
+    const char* address = nullptr;
+
+    switch (addr->sa_family) {
+        case AF_INET:
+            address = inet_ntop(AF_INET, &reinterpret_cast<const struct sockaddr_in *>(addr)->sin_addr, buffer, INET_ADDRSTRLEN);
+            break;
+        case AF_INET6:
+            address = inet_ntop(AF_INET6, &reinterpret_cast<const struct sockaddr_in6 *>(addr)->sin6_addr, buffer, INET6_ADDRSTRLEN);
+            break;
+    }
+
+    return address;
+}
+
 static void SetAddrStringHelper(napi_env env, napi_value addressObj, const char* key, sockaddr *addr) {
     if (addr == nullptr) return;
 
-    char dst_addr[INET6_ADDRSTRLEN] = {0};
-    const char* address = nullptr;
-
-    if (addr->sa_family == AF_INET) {
-        address = inet_ntop(AF_INET, &(((struct sockaddr_in*)addr)->sin_addr), dst_addr, INET_ADDRSTRLEN);
-    } else {
-        address = inet_ntop(AF_INET6, &(((struct sockaddr_in6*)addr)->sin6_addr), dst_addr, INET6_ADDRSTRLEN);
-    }
+    const char* address = GetIpAddress(addr);
 
     if (address) {
         napi_value temp;
@@ -55,6 +71,7 @@ napi_value libVersion(napi_env env, napi_callback_info info) {
 
     status = napi_create_string_utf8(env, pcap_lib_version(), NAPI_AUTO_LENGTH, &version);
     if (status != napi_ok) return nullptr;
+
     return version;
 }
 
@@ -65,12 +82,16 @@ napi_value deviceList(napi_env env, napi_callback_info info) {
     char error[PCAP_ERRBUF_SIZE] = {0};
     pcap_if_t *alldevs, *cur_dev;
 
-    if (pcap_findalldevs(&alldevs, error) == -1 || alldevs == NULL)
+    if (pcap_findalldevs(&alldevs, error) == -1 || alldevs == NULL) {
         napi_throw_error(env, NULL, error);
+        return nullptr;
+    }
 
     napi_value list;
-    if (napi_ok != napi_create_array(env, &list))
+    if (napi_ok != napi_create_array(env, &list)) {
         napi_throw_error(env, NULL, "Failed to create array for deviceList.");
+        return nullptr;
+    }
         
     int i = 0, j = 0;
     pcap_addr_t *cur_addr;
@@ -121,10 +142,70 @@ napi_value deviceList(napi_env env, napi_callback_info info) {
     return list;
 }
 
+napi_value findDevice(napi_env env, napi_callback_info info) {
+    size_t argc = 1;
+    napi_value argv[1], device;
+    napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+
+    if (argc < 1) {
+        napi_throw_error(env, nullptr, "Invalid number of arguments. Must provide 1 argument.");
+        return nullptr;
+    }
+
+    napi_valuetype type;
+    napi_typeof(env, argv[0], &type);
+
+    if (type != napi_string) {
+        napi_throw_error(env, nullptr, "The argument must be a string.");
+        return nullptr;
+    }
+
+    // Get the string value from the argument
+    size_t ipSize;
+    napi_get_value_string_utf8(env, argv[0], nullptr, 0, &ipSize);
+    std::string ip(ipSize, '\0');
+    napi_get_value_string_utf8(env, argv[0], &ip[0], ipSize + 1, nullptr);
+
+    char error[PCAP_ERRBUF_SIZE] = {0}, name[INET6_ADDRSTRLEN] = {0};
+    pcap_if_t *alldevs;
+
+    if (pcap_findalldevs(&alldevs, error) == -1 || alldevs == NULL) {
+        napi_throw_error(env, NULL, error);
+        return nullptr;
+    }
+
+    bool found = false;
+    pcap_if_t *cur_dev = nullptr;
+    pcap_addr_t *cur_addr = nullptr;
+
+    for (cur_dev = alldevs; cur_dev != nullptr; cur_dev = cur_dev->next) {
+        if (cur_dev->addresses == nullptr) continue;
+
+        for (cur_addr = cur_dev->addresses; cur_addr != nullptr; cur_addr = cur_addr->next) {
+            if (cur_addr->addr == nullptr) continue;
+
+            int family = cur_addr->addr->sa_family;
+            if (family != AF_INET && family != AF_INET6) continue;
+
+            std::string ipAddress = GetIpAddress(cur_addr->addr);
+            if (ip != ipAddress) continue;
+
+            napi_create_string_utf8(env, cur_dev->name, strlen(cur_dev->name), &device);
+            found = true;
+            break;
+        }
+
+        if (found) break;
+    }
+
+    pcap_freealldevs(alldevs);
+    return device;
+}
+
 napi_value Init(napi_env env, napi_value exports) {
     loadNpcap(env);
     
-    napi_value fnLibVersion, fnDeviceList;
+    napi_value fnLibVersion, fnDeviceList, fnFindDevice;
 
     // libVersion
     if (napi_ok != napi_create_function(env, "libVersion", NAPI_AUTO_LENGTH, libVersion, NULL, &fnLibVersion)) return NULL;
@@ -133,6 +214,10 @@ napi_value Init(napi_env env, napi_value exports) {
     // deviceList
     if (napi_ok != napi_create_function(env, "deviceList", NAPI_AUTO_LENGTH, deviceList, NULL, &fnDeviceList)) return NULL;
     if (napi_ok != napi_set_named_property(env, exports, "deviceList", fnDeviceList)) return NULL;
+
+    // findDevice
+    if (napi_ok != napi_create_function(env, "findDevice", NAPI_AUTO_LENGTH, findDevice, NULL, &fnFindDevice)) return NULL;
+    if (napi_ok != napi_set_named_property(env, exports, "findDevice", fnFindDevice)) return NULL;
 
     return exports;
 }
